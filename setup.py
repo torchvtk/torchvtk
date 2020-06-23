47a7b62d37c92a786bf34315c3378fdc4084445c
Dominik Engel <dominik.engel@mail.de>
2020-06-23 15:24:36 +0200

    add dependencies, hopefully fix pypi package name issue

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup, find_packages
 setup(
-    name="torch_vtk",
+    name="torch-vtk",
+    description="Efficient data loading and visualization for volumes in PyTorch",
     version="0.1",
-    packages=find_packages(),
+    packages=['torch_vtk'],
+    install_requires=[
+        'torch', 'torchvision', 'numpy', 'matplotlib'
+    ],
+    dependency_links=[
+        'https://github.com/aliutkus/torchsearchsorted.git@master#egg=torchsearchsorted',
+        'https://github.com/aliutkus/torchinterp1d.git@master#egg=torchinterp1d'
+    ]
 )
