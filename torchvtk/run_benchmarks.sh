942624c8f9813fea3f6171ee862475e89364f87f
Dominik Engel <dominik.engel@mail.de>
2020-06-29 18:51:32 +0200

    remove test script

--- torchvtk/run_benchmarks.sh
+++
@@ -1,4 +0,0 @@
-python preprocessing/converters/torch_to_hdf5.py /run/media/dome/Data/data/Volumes/tvdls /run/media/dome/Data/data/Volumes/tvdls_lzf.h5 -compression lzf
-python preprocessing/converters/torch_to_hdf5.py /run/media/dome/Data/data/Volumes/tvdls /run/media/dome/Data/data/Volumes/tvdls_gzip.h5 -compression gzip
-python benchmarks/benchmark_cq500_hdf5.py /run/media/dome/Data/data/Volumes/tvdls_lzf.h5 -plot ~/Pictures/tvdls_hdf5_lzf_results.png
-python benchmarks/benchmark_cq500_hdf5.py /run/media/dome/Data/data/Volumes/tvdls_gzip.h5 -plot ~/Pictures/tvdls_hdf5_gzip_results.png
