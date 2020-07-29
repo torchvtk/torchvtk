a289cedd10adf574814eba1fcb56f6576791793e
marc.mezger <marc.mezger@uni-ulm.de>
2020-07-29 12:07:51 +0200

    Reenable all tests.

--- torchvtk/test/test_augmentations.py
+++ torchvtk/test/test_augmentations.py
@@ -8,42 +8,42 @@ from torchvtk.augmentation.DictTransform import BlurDictTransform, NoiseDictTran
 
 def main():
     # import test image
-    # file = load_file()
-    #
-    # # double
-    # file["vol"] = torch.stack([file["vol"], file["vol"]], dim=0)
-    # view_batch(file["vol"][0, ...], width=512, height=512)
-    #
-    # # Test Noise Transform.
-    # tfms = NoiseDictTransform(std_deviation=0.01, device="cpu", apply_on=["vol"])
-    # tmp = tfms(file)
-    # view_batch(tmp["vol"].squeeze(), width=512, height=512)
-    # del tfms, file
-    #
-    # # Test Noise Transform GPU.
-    # file = load_file()
-    #
-    # tfms = NoiseDictTransform(device="cuda", apply_on=["vol"], std_deviation=0.01)
-    # tmp = tfms(file)
-    # view_batch(tmp["vol"].squeeze(), width=512, height=512)
-    # del tfms, file
-    #
-    # # test for gaussian blur cpu
-    # file = load_file()
-    # # double
-    # file["vol"] = torch.stack([file["vol"], file["vol"]], dim=0)
-    # tfms = BlurDictTransform(apply_on=["vol"], device="cpu", channels=1, kernel_size=(3, 3, 3), sigma=1)
-    # tmp = tfms(file)
-    # view_batch(tmp["vol"].squeeze(), width=512, height=512)
-    # del tfms, file
-    #
-    # # test for gaussian blur gpu
-    # file = load_file()
-    # tfms = BlurDictTransform(apply_on=["vol"], device="cuda", channels=1, kernel_size=(3, 3, 3), sigma=1)
-    # blur_gpu = tfms(file)
-    # view_batch(blur_gpu["vol"].squeeze(), width=512, height=512)
-    # file["vol"] = file["vol"].to("cpu")
-    # del tfms, file
+    file = load_file()
+
+    # double
+    file["vol"] = torch.stack([file["vol"], file["vol"]], dim=0)
+    view_batch(file["vol"][0, ...], width=512, height=512)
+
+    # Test Noise Transform.
+    tfms = NoiseDictTransform(std_deviation=0.01, device="cpu", apply_on=["vol"])
+    tmp = tfms(file)
+    view_batch(tmp["vol"].squeeze(), width=512, height=512)
+    del tfms, file
+
+    # Test Noise Transform GPU.
+    file = load_file()
+
+    tfms = NoiseDictTransform(device="cuda", apply_on=["vol"], std_deviation=0.01)
+    tmp = tfms(file)
+    view_batch(tmp["vol"].squeeze(), width=512, height=512)
+    del tfms, file
+
+    # test for gaussian blur cpu
+    file = load_file()
+    # double
+    file["vol"] = torch.stack([file["vol"], file["vol"]], dim=0)
+    tfms = BlurDictTransform(apply_on=["vol"], device="cpu", channels=1, kernel_size=(3, 3, 3), sigma=1)
+    tmp = tfms(file)
+    view_batch(tmp["vol"].squeeze(), width=512, height=512)
+    del tfms, file
+
+    # test for gaussian blur gpu
+    file = load_file()
+    tfms = BlurDictTransform(apply_on=["vol"], device="cuda", channels=1, kernel_size=(3, 3, 3), sigma=1)
+    blur_gpu = tfms(file)
+    view_batch(blur_gpu["vol"].squeeze(), width=512, height=512)
+    file["vol"] = file["vol"].to("cpu")
+    del tfms, file
 
     # Cropping CPU.
     file = load_file()
@@ -52,13 +52,13 @@ def main():
     view_batch(tmp["vol"].squeeze(), tmp["mask"].squeeze(), width=512, height=512)
     del tfms, file
 
-    # # Cropping GPU.
-    # file = load_file()
-    # # double
-    # file["vol"] = torch.stack([file["vol"], file["vol"]], dim=0)
-    # tfms = CroppingTransform(device="cuda", apply_on=["vol"], dtype=torch.float32)
-    # tmp = tfms(file)
-    # view_batch(tmp["vol"].squeeze(), width=512, height=512)
+    # Cropping GPU.
+    file = load_file()
+    # double
+    file["vol"] = torch.stack([file["vol"], file["vol"]], dim=0)
+    tfms = CroppingTransform(device="cuda", apply_on=["vol"], dtype=torch.float32)
+    tmp = tfms(file)
+    view_batch(tmp["vol"].squeeze(), width=512, height=512)
 
 
 def load_file():
